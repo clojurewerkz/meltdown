c04c3ae3ad01d81f6056a398e3fa41bc1c2ca476
Michael Klishin <michael@defprotocol.org>
2013-05-19 01:40:14 +0400

    Add a test for the root reactor

--- test/clojurewerkz/meltdown/reactor_test.clj
+++ test/clojurewerkz/meltdown/reactor_test.clj
@@ -9,17 +9,27 @@
 (deftest test-basic-delivery-over-non-root-reactor
   (let [latch (CountDownLatch. 1)
         r     (mr/create)
-        sel   ($ "events.silly")
-        key   "events.silly"
         data  {:event "delivered"}
         res   (atom nil)]
-    (mr/on r sel (fn [event]
-                   (reset! res event)
-                   (.countDown latch)))
-    (.start (Thread. (fn []
-                       (Thread/sleep 100)
-                       (mr/notify r key data))))
+    (mr/on r ($ "events.silly") (fn [event]
+                                  (reset! res event)
+                                  (.countDown latch)))
+    (mr/notify r "events.silly" data)
     (.await latch 5 TimeUnit/SECONDS)
+    (let [d @res]
+      (is (:id d))
+      (is (= {} (:headers d)))
+      (is (= "delivered" (get-in d [:data :event]))))))
+
+(deftest test-basic-delivery-over-root-reactor
+  (let [latch (CountDownLatch. 1)
+        data  {:event "delivered"}
+        res   (atom nil)]
+    (mr/on ($ "events.silly") (fn [event]
+                                (reset! res event)
+                                (.countDown latch)))
+    (mr/notify "events.silly" data)
+    (.await latch 3 TimeUnit/SECONDS)
     (let [d @res]
       (is (:id d))
       (is (= {} (:headers d)))
