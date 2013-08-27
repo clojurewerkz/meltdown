8ddc5abe0b5e291ebca2a1be7ba1fab58a795426
Alex Petrov <alexp@coffeenco.de>
2013-08-27 18:11:24 +0200

    Add stream operations

---
+++ src/clojure/clojurewerkz/meltdown/streams.clj
@@ -0,0 +1,65 @@
+(ns clojurewerkz.meltdown.streams
+  (:import [reactor.core.composable.spec Streams]
+           [reactor.core.composable Stream Deferred]
+           [reactor.function Function Predicate]
+           [reactor.tuple Tuple2]
+           clojure.lang.IFn)
+  (:require [clojurewerkz.meltdown.consumers :as mc]))
+
+(defn ^Function fn->function
+  "Instantiates a reactor consumer from a Clojure
+   function"
+  [^IFn f]
+  (reify Function
+    (apply [this a]
+      (f a))))
+
+(defn ^Predicate fn->predicate
+  "Instantiates a reactor consumer from a Clojure
+   function"
+  [^IFn f]
+  (proxy [Predicate] []
+    (test [a]
+      (f a))))
+
+(defn accept
+  [^Stream stream value]
+  (.accept stream value))
+
+(defn- maybe-compose
+  [deferred-or-stream]
+  (if (instance? Deferred deferred-or-stream)
+    (.compose deferred-or-stream)
+    deferred-or-stream))
+
+(defn map*
+  [f deferred-or-stream]
+  (.map (maybe-compose deferred-or-stream) (fn->function f)))
+
+(defn filter*
+  [f deferred-or-stream]
+  (.filter (maybe-compose deferred-or-stream) (fn->predicate f)))
+
+(defn batch*
+  [i deferred-or-stream]
+  (map* #(into [] %)
+        (.collect (.batch (maybe-compose deferred-or-stream) i))))
+
+(defn reduce*
+  [f deferred-or-stream]
+  (.reduce (maybe-compose deferred-or-stream) (fn->function
+                                               (fn [^Tuple2 tuple]
+                                                 (let [value (.getT1 tuple)
+                                                       acc (.getT2 tuple)]
+                                                   (f acc value))))))
+
+(defn consume
+  [^Stream stream f]
+  (.consume stream
+            (mc/from-fn-raw f)))
+
+(defn create
+  []
+  (let [spec (Streams/defer)]
+    (.synchronousDispatcher spec)
+    (.get spec)))
---
+++ test/clojurewerkz/meltdown/streams_test.clj
@@ -0,0 +1,71 @@
+(ns clojurewerkz.meltdown.streams-test
+  (:require [clojure.test :refer :all])
+  (:use clojurewerkz.meltdown.streams
+        clojure.test))
+
+(alter-var-root #'*out* (constantly *out*))
+
+(deftest basic-stream-map-test
+  (let [channel (create)
+        stream  (map* inc channel)
+        stream2  (map* #(+ 2 %) stream)
+        stream3  (map* #(+ 3 %) stream2)
+
+        res (atom {})]
+
+    (consume stream (fn [v] (swap! res assoc :first v)))
+    (consume stream2 (fn [v] (swap! res assoc :second v)))
+    (consume stream3 (fn [v] (swap! res assoc :third v)))
+
+    (accept channel 1)
+    (let [d @res]
+      (is (= 2 (:first d)))
+      (is (= 4 (:second d)))
+      (is (= 7 (:third d))))))
+
+
+(deftest basic-stream-filter-test
+  (let [channel (create)
+        even    (filter* even? channel)
+        odd     (filter* odd? channel)
+        res     (atom {})]
+
+    (consume even (fn [v] (swap! res assoc :even v)))
+    (consume odd (fn [v] (swap! res assoc :odd v)))
+
+    (accept channel 1)
+    (accept channel 2)
+
+    (let [d @res]
+      (is (= 1 (:odd d)))
+      (is (= 2 (:even d))))))
+
+
+(deftest batch-test
+  (let [channel     (create)
+        incrementer (map* inc channel)
+        batcher     (batch* 3 incrementer)
+        res         (atom nil)]
+
+    (consume batcher (fn [i] (reset! res i)))
+
+    (accept channel 1)
+    (is (nil? @res))
+    (accept channel 1)
+    (accept channel 1)
+
+    (is (= [2 2 2] @res))))
+
+
+(deftest basic-stream-reduce-test
+  (let [channel (create)
+        stream  (reduce* #(+ (or %1 0) %2) channel)
+        res     (atom nil)]
+
+    (consume stream #(reset! res %))
+
+    (accept channel 1)
+    (accept channel 2)
+    (accept channel 3)
+
+    (is (= 6 @res))))
