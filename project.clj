f5cad5aabc7ab338f170521f26d17f7d583bd6ba
Alex Petrov <alexp@coffeenco.de>
2013-08-30 22:52:30 +0200

    Back to snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojurewerkz/meltdown "1.0.0-alpha3"
+(defproject clojurewerkz/meltdown "1.0.0-alpha4-SNAPSHOT"
   :description "Clojure interface to Reactor, an event-driven programming toolkit for the JVM"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clojurewerkz/support "0.15.0"]
