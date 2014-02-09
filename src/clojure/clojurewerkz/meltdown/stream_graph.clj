394d39bbab32dedfcef8b25ce3b3a412128c60ae
Michael Klishin <michael@clojurewerkz.org>
2014-02-09 13:39:14 +0400

    License Headers

--- src/clojure/clojurewerkz/meltdown/stream_graph.clj
+++ src/clojure/clojurewerkz/meltdown/stream_graph.clj
@@ -1,4 +1,4 @@
-;; Copyright (c) 2013 The ClojureWerkz team and contributors.
+;; Copyright (c) 2013-2014 Michael S. Klishin, Alex Petrov, and the ClojureWerkz Team.
 ;;
 ;; Licensed under the Apache License, Version 2.0 (the "License");
 ;; you may not use this file except in compliance with the License.
