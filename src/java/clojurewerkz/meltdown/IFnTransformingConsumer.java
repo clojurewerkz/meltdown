2f2b0015fad5503998557367f6562bc904f572b2
Michael Klishin <mklishin@gopivotal.com>
2014-02-22 13:57:53 +0400

    Cosmetics

--- src/java/clojurewerkz/meltdown/IFnTransformingConsumer.java
+++ src/java/clojurewerkz/meltdown/IFnTransformingConsumer.java
@@ -20,16 +20,16 @@ import reactor.function.Consumer;
 public class IFnTransformingConsumer<T> implements Consumer<T> {
 
   private final IFn acceptor;
-	private final IFn transformer;
+  private final IFn transformer;
 
   public IFnTransformingConsumer(IFn acceptor, IFn transformer) {
     this.acceptor = acceptor;
-		this.transformer = transformer;
+    this.transformer = transformer;
   }
 
   @Override
   public void accept(T value) {
-		acceptor.invoke(transformer.invoke(value));
+    acceptor.invoke(transformer.invoke(value));
   }
 
 }
