347c9271ec10397442256b17227ae3815d13d2c6
Michael Klishin <michael@clojurewerkz.org>
2014-03-27 09:31:28 +0400

    Master is now 1.7 snapshot

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
                  [org.projectreactor/reactor-core "1.1.0.M2"]]
   :profiles {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
-             :master {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
+             :master {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}
              :dev {:resource-paths ["test/resources"]
                    :dependencies   [[com.lmax/disruptor "3.2.1"]]
                    :plugins [[codox "0.6.6"]]
