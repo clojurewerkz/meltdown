cc8bc5c531aabe6f6cd3772da82c2c85f49c4b78
Michael Klishin <michael@clojurewerkz.org>
2014-06-05 21:54:04 +0400

    Codox 0.8.9

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
              :master {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}
              :dev {:resource-paths ["test/resources"]
                    :dependencies   [[com.lmax/disruptor "3.2.1"]]
-                   :plugins [[codox "0.8.8"]]
+                   :plugins [[codox "0.8.9"]]
                    :codox {:sources ["src/clojure"]
                            :output-dir "doc/api"}}}
   :aliases {"all" ["with-profile" "dev:dev,1.4:dev,1.5:dev,master"]}
