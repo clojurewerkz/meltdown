95c82d30f1d5102154ca9a3743d2f7766eddc0e8
Michael Klishin <michael@clojurewerkz.org>
2014-08-31 19:06:24 +0400

    Back to dev version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojurewerkz/meltdown "1.1.0-beta1"
+(defproject clojurewerkz/meltdown "1.1.0-beta2-SNAPSHOT"
   :description "Clojure interface to Reactor, an event-driven programming toolkit for the JVM"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.projectreactor/reactor-core "1.1.4.RELEASE"]]
