a9aa27b10190c7d886b00d7dcafb58bcf2928a12
Michael Klishin <michael@clojurewerkz.org>
2014-09-06 18:45:43 +0400

    Clojure 1.7.0-alpha2

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
                  [org.projectreactor/reactor-core "1.1.4.RELEASE"]]
   :profiles {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha1"]]}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha2"]]}
              :master {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}
              :dev {:resource-paths ["test/resources"]
                    :dependencies   [[com.lmax/disruptor "3.3.0"]]
