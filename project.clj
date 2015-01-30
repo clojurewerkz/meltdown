7b49ecdcb02625653f5a7cd15bb6ae1094e091a9
Michael Klishin <michael@clojurewerkz.org>
2015-01-30 23:44:47 +0300

    Ditto

--- project.clj
+++ project.clj
@@ -12,7 +12,7 @@
                    :plugins [[codox "0.8.10"]]
                    :codox {:sources ["src/clojure"]
                            :output-dir "doc/api"}}}
-  :aliases {"all" ["with-profile" "dev:dev,1.4:dev,1.5:dev,1.7:dev,master"]}
+  :aliases {"all" ["with-profile" "dev:dev,1.7:dev,master"]}
   :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
                              :snapshots false
                              :releases {:checksum :fail}}
